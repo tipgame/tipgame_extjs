cb44771ac139f7f9754c31b9214ba77ac8f14f66
jens <jensburkhardt@web.de>
2018-04-07 00:15:17 +0200

    delete extjs

--- client/TipgameApp/app/model/session/SessionModel.js
+++
@@ -1,5 +0,0 @@
-Ext.define('TipgameApp.model.session.SessionModel', {
- extend: 'Ext.data.Model',
-
- fields: [{token: "token", type:'string'}, {id: 'id', type: 'int'}]
- });
--- client/TipgameApp/app/model/user/UserModel.js
+++
@@ -1,8 +0,0 @@
-Ext.define('TipgameApp.model.user.UserModel', {
-    extend: 'Ext.data.Model',
-    fields: ['firstname', 'lastname', 'username', 'userId', 'email'],
-    proxy: {
-            type: 'rest',
-            url : 'http://localhost:8080/user'
-        }
-});
