700ff29e95254755e8f225c46094b8c3596dcdf3
jens <jensburkhardt@web.de>
2018-04-07 09:27:58 +0200

    switch to extjs gpl version

--- client/ext/licenses/file-header.js
+++ client/ext/licenses/file-header.js
@@ -1,16 +1,21 @@
 /*
-This file is part of Ext JS 6.0.0.640
+This file is part of Ext JS 6.2.0.981
 
-Copyright (c) 2011-2015 Sencha Inc
+Copyright (c) 2011-2016 Sencha Inc
 
 Contact:  http://www.sencha.com/contact
 
-This version of Sencha Ext JS is licensed commercially for a limited period for evaluation 
-purposes only. Production use or use beyond the applicable evaluation period is prohibited 
-under this license.
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as
+published by the Free Software Foundation and appearing in the file LICENSE included in the
+packaging of this file.
 
-If your trial has expired, please contact the sales department at http://www.sencha.com/contact.
+Please review the following information to ensure the GNU General Public License version 3.0
+requirements will be met: http://www.gnu.org/copyleft/gpl.html.
 
-Version: 6.0.0.640 Build date: 2015-06-28 13:02:46 (134671ed5b6bf074e014e83502b615cd2e26a515)
+If you are unsure which license is appropriate for your use, please contact the sales department
+at http://www.sencha.com/contact.
+
+Version: 6.2.0.981 Build date: 2016-08-31 14:49:44 (08dbbd0ec0b8bc0e014d725fdb7d9650d510b343)
 
 */
